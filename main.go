7b2f24053e98451262391f9720dbf9269782ff66
sungmuk Kang <miranaky@gmail.com>
2021-06-28 16:24:06 +0900

    explorer our coin at web browser create / as home page

--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 
 	"github.com/miranaky/kaengkaengcoin/blockchain"
 )
 
-func main() {
-	chain := blockchain.GetBlockChain()
-	chain.AddBlock("Second block")
-	chain.AddBlock("Thrid block")
-	chain.AddBlock("Fourth block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s \n", block.Data)
-		fmt.Printf("Hash: %s \n", block.Hash)
-		fmt.Printf("Prev Hash: %s \n", block.PrevHash)
+const (
+	port         string = ":4000"
+	templatesDir        = "templates/"
+)
 
-	}
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockChain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func main() {
+	templates = template.Must(template.ParseGlob(templatesDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templatesDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening at http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+
 }
