a37d0c804addf80425f0943094d8aff878c73eb9
sungmuk Kang <miranaky@gmail.com>
2021-06-30 19:07:36 +0900

    CLI create cli for running kaengkaeng coin

---
+++ cli/cli.go
@@ -0,0 +1,50 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/miranaky/kaengkaengcoin/explorer"
+	"github.com/miranaky/kaengkaengcoin/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome to KaengKaeng Coin!\n\n")
+	fmt.Printf("Please use the folllowing flags:\n\n")
+	fmt.Printf("	-mode :	Choose between 'html','rest' or 'both'\n")
+	fmt.Printf("	-port :	Set port of the server\n\n")
+	os.Exit(0)
+}
+
+var port int
+var mode string
+var help bool
+
+func initFlags() {
+	flag.IntVar(&port, "port", 4000, "Set PORT of the server")
+	flag.StringVar(&mode, "mode", "rest", "Choose between 'html','rest' or 'both'")
+	flag.BoolVar(&help, "help", false, "Show the kanegkaeng coin usage")
+}
+
+func Start() {
+
+	initFlags()
+	flag.Parse()
+
+	if help {
+		usage()
+	}
+
+	switch mode {
+	case "rest":
+		rest.Start(port)
+	case "html":
+		explorer.Start(port)
+	case "both":
+		go explorer.Start(port + 1)
+		rest.Start(port)
+	default:
+		usage()
+	}
+}
